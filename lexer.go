ee63c92f612368f7d7f3799b96cd587310e01d16
Ryan Chipman <ryan@ryanchipman.com>
2015-11-04 17:40:02 -0500

    Implement generic lexer
    Implement a lexer using the methodology and code at cuddle.googlecode.com/hg/talk/lex.html

---
+++ lexer.go
@@ -0,0 +1,82 @@
+package gon3
+
+import (
+	"fmt"
+)
+
+type lexer struct {
+	name   string
+	input  string
+	start  int
+	pos    int
+	width  int
+	tokens chan token
+}
+
+func (l *lexer) run() {
+	for state := lexDocument; state != nil; {
+		state = state(l)
+	}
+	close(l.tokens)
+}
+
+func (l *lexer) emit(t tokenType) {
+	l.tokens <- token{t, l.input[l.start:l.pos]}
+	l.start = l.pos
+}
+
+func lex(name, input string) (*lexer, chan tokens) {
+	l := &lexer{
+		name:   name,
+		input:  input,
+		tokens: make(chan tokens),
+	}
+	go l.run()
+	return l, l.tokens
+}
+
+func (l *lexer) next() int {
+	if l.pos >= len(l.input) {
+		l.width = 0
+		return eof
+	}
+	r, l.width = utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return r
+}
+
+func (l *lexer) ignore() {
+	l.start = l.pos
+}
+
+func (l *lexer) backup() {
+	l.pos -= l.width
+}
+
+func (l *lexer) peek() int {
+	r := l.next()
+	l.backup()
+	return r
+}
+
+func (l *lexer) accept(valid string) bool {
+	if strings.IndexRune(valid, l.next()) >= 0 {
+		return true
+	}
+	l.backup()
+	return false
+}
+
+func (l *lexer) acceptRun(valid string) {
+	for strings.IndexRune(valid, l.next()) >= 0 {
+	}
+	l.backup()
+}
+
+func (l *lexer) errorf(format string, args ...interface{}) stateFn {
+	l.tokens <- token{
+		tokenError,
+		fmt.Sprintf(format, args),
+	}
+	return nil
+}
