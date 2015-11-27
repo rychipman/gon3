197f3a3ce867849f6ecaf9fa9091de2e7ee1cd0b
Ryan Chipman <ryan@ryanchipman.com>
2015-11-27 15:51:43 -0500

    Implement data structures into which data will be parsed

---
+++ rdf.go
@@ -0,0 +1,25 @@
+package gon3
+
+// This must be a full (i.e. not relative IRI)
+type IRI string
+
+// see http://www.w3.org/TR/rdf11-concepts/#dfn-blank-node
+// A blank node can have an internal iri
+type BlankNode IRI
+
+// see http://www.w3.org/TR/rdf11-concepts/#dfn-literal
+type Literal struct {
+	LexicalForm string
+	DatatypeIRI IRI
+	LanguageTag
+}
+
+// see http://www.w3.org/TR/rdf11-concepts/#dfn-rdf-triple
+type Triple struct {
+	Subject   IRI // TODO: allow to be a blank node
+	Predicate IRI
+	Object    IRI // TODO: allow to be a blank node or a literal
+}
+
+// An RDF graph is a set of RDF triples
+type Graph []*Triple
