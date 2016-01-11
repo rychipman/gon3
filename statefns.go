8571bc975d867c619af416466b96bb3409e52fcb
Ryan Chipman <ryan@ryanchipman.com>
2016-01-11 16:38:07 -0500

    start writing statefns

--- statefns.go
+++ statefns.go
@@ -7,25 +7,6 @@ import (
 type stateFn func(*lexer) stateFn
 
 const (
-	atPrefix       = "@prefix"
-	atBase         = "@base"
-	endStatement   = "."
-	endProperty    = ";"
-	listSeparator  = ","
-	beginPropList  = "["
-	endPropList    = "]"
-	beginFormula   = "{"
-	endFormula     = "}"
-	beginPathlist  = "("
-	endPathlist    = ")"
-	equals         = "="
-	implies        = "=>"
-	reverseImplies = "<="
-	eof            = -1
-	// TODO: add consts for ^,!,^^,a,<,>
-)
-
-const (
 	runAlphabet     = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	runWhitespace   = " \n\t\v\f\r"
 	runDigits       = "0123456789"
@@ -37,53 +18,39 @@ const (
 )
 
 func lexDocument(l *lexer) stateFn {
-	for {
-		// I am a state function. I return another state function
-		if l.next() == eof {
-			break
-		}
+	l.acceptRun(runWhitespace)
+	switch l.next() {
+	// prefix/base directives
+	case "@prefix":
+		// lex prefix
+	case "@base":
+		// lex base
+	// subject constructions
+	case "[":
+		// lex blank node prop list
+	case "(":
+		// lex collection
+	case "_":
+		// lex bnode label
+	case "<":
+		// lex iri
+	default:
+		// lex pname
 	}
-	// reached EOF
-	// TODO: emit error token if unparsed text in doc
 }
 
-func lexWhitespace(l *lexer) stateFn {
-
+func lexPrefix(l *lexer) stateFn {
+	// TODO: implement
 }
 
-func lexNumericLiteral(l *lexer) stateFn {
-	// TODO
+func lexBase(l *lexer) stateFn {
+	// TODO: implement
 }
 
-func lexString(l *lexer) stateFn {
-	// TODO
+func lexBlankNodePropertyList(l *lexer) stateFn {
+	// TODO: implement
 }
 
-func lexVariable(l *lexer) stateFn {
-	// TODO
-}
-
-func lexLangcode(l *lexer) stateFn {
-	// TODO
-}
-
-func lexQname(l *lexer) stateFn {
-	if l.input[l.pos] != l.qnamePrefix {
-		l.errorf("Expected qname prefix %s, got %s", l.qnamePrefix, l.input[l.pos])
-		return nil
-	}
-	l.pos += len(l.qnamePrefix)
-	l.acceptRun(runQname)
-	l.emit(tokenQname)
-	return lexWhitespace
-}
-
-func lexBarename(l *lexer) stateFn {
-	// TODO
-}
-
-func lexExplicitURI(l *lexer) stateFn {
-	l.acceptRun(runURI)
-	l.emit(tokenExplicitURI)
-	return lexWhitespace
+func lexCollection(l *lexer) stateFn {
+	// TODO: implement
 }
