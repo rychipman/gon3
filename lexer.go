bff2419fec98e3d3de5721e47e783a4448b6968d
Ryan Chipman <ryan@ryanchipman.com>
2016-01-24 10:59:30 -0500

    Add typeMockLexer for mocking a lexer based on token types

--- lexer.go
+++ lexer.go
@@ -25,3 +25,24 @@ func (m *mockLexer) NextToken() easylex.Token {
 	m.pos += 1
 	return ret
 }
+
+type typeMockLexer struct {
+	types []easylex.TokenType
+	pos   int
+}
+
+func newTypeMockLexer(args ...easylex.TokenType) *typeMockLexer {
+	return &typeMockLexer{
+		types: args,
+		pos:   0,
+	}
+}
+
+func (m *typeMockLexer) NextToken() easylex.Token {
+	typ := m.types[m.pos]
+	m.pos += 1
+	return easylex.Token{
+		typ,
+		"n/a",
+	}
+}
