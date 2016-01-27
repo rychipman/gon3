6db66da1a527bb633253a9db0824a39c83cfd036
Ryan Chipman <ryan@ryanchipman.com>
2016-01-27 16:04:13 -0500

    Add basic string funcs to rdf datatypes

--- rdf.go
+++ rdf.go
@@ -1,6 +1,7 @@
 package gon3
 
 import (
+	"fmt"
 	"net/url"
 )
 
@@ -11,6 +12,10 @@ type RDFTerm interface {
 // This must be a full (i.e. not relative IRI)
 type IRI struct {
 	url *url.URL
+}
+
+func (i IRI) String() string {
+	return fmt.Sprintf("<%s>", i.url)
 }
 
 func newIRIFromString(s string) (IRI, error) {
@@ -30,11 +35,26 @@ type BlankNode struct {
 	Label string
 }
 
+func (b BlankNode) String() string {
+	label := b.Label
+	if label == "" {
+		label = "anonbnode"
+	}
+	return fmt.Sprintf("_:%s", label)
+}
+
 // see http://www.w3.org/TR/rdf11-concepts/#dfn-literal
 type Literal struct {
 	LexicalForm string
 	DatatypeIRI IRI
 	LanguageTag string
+}
+
+func (l Literal) String() string {
+	if l.LanguageTag == "" {
+		return fmt.Sprintf("%q^^%s", l.LexicalForm, l.DatatypeIRI)
+	}
+	return fmt.Sprintf("%q^^%s@%s", l.LexicalForm, l.DatatypeIRI, l.LanguageTag)
 }
 
 // see http://www.w3.org/TR/rdf11-concepts/#dfn-rdf-triple
@@ -44,5 +64,20 @@ type Triple struct {
 	Object    RDFTerm
 }
 
+func (t *Triple) String() string {
+	return fmt.Sprintf("%s %s %s .", t.Subject, t.Predicate, t.Object)
+}
+
 // An RDF graph is a set of RDF triples
 type Graph []*Triple
+
+func (g Graph) String() string {
+	str := ""
+	for i, t := range g {
+		if i > 0 {
+			str += "\n"
+		}
+		str = fmt.Sprintf("%s%s", str, t)
+	}
+	return str
+}
